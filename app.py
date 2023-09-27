8c0b803a055d9a467b59db275cc6effc1082e787
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-27 20:53:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,15 +25,15 @@ HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 repo_id = os.environ.get('repo_id')
 
 llm = HuggingFaceHub(repo_id=repo_id,
-                     model_kwargs={"min_length":100,
-                                   "max_new_tokens":1024, "do_sample":True,
+                     model_kwargs={"min_length":1024,
+                                   "max_new_tokens":5632, "do_sample":True,
                                    "temperature":0.1,
                                    "top_k":50,
-                                   "top_p":0.95, "eos_token_id":49155})
+                                   "top_p":0.95, "eos_token_id":49155}) 
 
 prompt_template = """You are a very helpful AI assistant. Please response to the user's input question with as many details as possible.
 Question: {user_question}
-Helpufl AI AI Repsonse:
+Helpful AI Repsonse:
 """  
 llm_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(prompt_template))
 
@@ -41,8 +41,8 @@ user_query = st.text_input("Enter your query here:")
 with st.spinner("AI Thinking...Please wait a while to Cheers!"):    
     if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():         
         initial_response=llm_chain.run(user_query)
-        temp_ai_response_1=initial_response.partition('<|end|>\n<|user|>\n')[0]
-        temp_ai_response_2=temp_ai_response_1.replace('<|end|>\n<|assistant|>\n', '') 
-        final_ai_response=temp_ai_response_2.replace('<|end|>\n<|system|>\n', '')         
+        #temp_ai_response_1=initial_response.partition('<|end|>\n<|user|>\n')[0]
+        #temp_ai_response_2=temp_ai_response_1.replace('<|end|>\n<|assistant|>\n', '') 
+        #final_ai_response=temp_ai_response_2.replace('<|end|>\n<|system|>\n', '')         
         st.write("AI Response:")
-        st.write(final_ai_response)
+        st.write(initial_response)
